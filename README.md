75fadb4b3dca3aaf3c86211ec0a3d537687c1287
Joshua McKinney <joshka.github@joshka.net>
2013-03-23 01:48:57 +1100

    change to node server for speed on heroku

---
+++ README.md
@@ -0,0 +1,1 @@
+counts the WTFs
---
+++ server.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var send = require('send');
+var url = require('url');
+var path = require('path')
+var app = require('express')()
+
+var server = http.createServer(app).listen(process.env.PORT || 8080)
+
+app.use(function(req, res) {
+  function error(err) {
+    res.statusCode = err.status || 500;
+    res.end(err.message);
+  }
+  function redirect() {
+    res.statusCode = 301;
+    res.setHeader('Location', req.url + '/');
+    res.end('Redirecting to ' + req.url + '/');
+  }
+  send(req, url.parse(req.url).pathname)
+    .root(__dirname))
+    .on('error', error)
+    .on('directory', redirect)
+    .pipe(res);
+})
