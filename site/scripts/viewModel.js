af463136f79541bee8b50ed2ab5c0969cbdd7450
Joshua McKinney <joshka.github@joshka.net>
2013-03-24 03:37:30 +1100

    Save last WTF date properly to localstorage

--- site/scripts/viewModel.js
+++ site/scripts/viewModel.js
@@ -2,23 +2,22 @@ var ko = require('knockout-client'),
 	datejs = require('datejs');
 ko.mapping = require('knockout-mapping');
 
-function ViewModel(wtfs) {
+function ViewModel(wtfs, lastWtf) {
 	this.wtfCount = ko.observable(wtfs);
+	this.lastWtf = ko.observable(lastWtf);
+	this.lastWtfText = ko.computed(function(argument) {
+		return this.lastWtf() || 'never';
+	}, this);
+
 	ko.computed(function() {
 		localStorage.setItem('wtfs', this.wtfCount());
-		localStorage.setItem('lastWtf', new Date());
+		localStorage.setItem('lastWtf', this.lastWtf());
 	}, this).extend({throttle: 1});
-	this.lastWtf = ko.computed(function() {
-		var c = this.wtfCount();
-		if (!c)
-			return "never";
-		return new Date().toString('F');
-	}, this);
 }
 
 ViewModel.prototype.wtf = function() {
-	var currentWtfs = this.wtfCount();
-	this.wtfCount(currentWtfs + 1);
+	this.wtfCount(this.wtfCount() + 1);
+	this.lastWtf(new Date().toString('F'));
 }
 
 ViewModel.prototype.reset = function() {
@@ -27,6 +26,8 @@ ViewModel.prototype.reset = function() {
 
 var storedVal = localStorage.getItem('wtfs');
 var wtfs = parseInt(storedVal) || 0;
-var vm = new ViewModel(wtfs);
+var lastWtf = localStorage.getItem('lastWtf');
+
+var vm = new ViewModel(wtfs, lastWtf);
 
 module.exports = vm;
